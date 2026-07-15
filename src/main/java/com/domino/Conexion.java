1acafe73f18703e16299d44c25e0531e4353632b
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-15 08:55:48 -0500

    commit - Conexion / .

--- src/main/java/com/domino/Conexion.java
+++ src/main/java/com/domino/Conexion.java
@@ -1,4 +1,19 @@
 package com.domino;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexion {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/domino_db";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "Tapiero123";
+
+    public static Connection conectar() throws Exception {
+
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }   // clase conexion a db !
 }
