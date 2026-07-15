6a4b75ca9d86b3d885bd8c6c64b65c5245333ec0
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-15 09:22:32 -0500

    commit - Main .

--- src/main/java/com/domino/Main.java
+++ src/main/java/com/domino/Main.java
@@ -1,17 +1,31 @@
 package com.domino;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Domino domino = new Domino();
+
+            domino.barajar();
+
+            DominoDAO dao = new DominoDAO();
+
+            System.out.println();
+            System.out.println("DOMINO");
+            System.out.println("----------------------");
+
+            for (Ficha ficha : domino.getFichas()) {
+
+                System.out.println(ficha);
+                dao.guardar(ficha);
+
+            }
+
+            dao.mostrar();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }   
     }
 }
